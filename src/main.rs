e496e9cb23564d0d126e8967e40cabd00fa5e3c9
Somyajeet <isomya13@gmail.com>
2024-05-29 21:12:51 +0530

    feat: extract cpu usage

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
-use axum::{routing::get, Router};
-
-
+use axum::{extract::State, routing::get, Router};
+use sysinfo::System;
+use std::sync::Mutex;use std::sync::Arc;
 #[tokio::main]
 async fn main() {
 
     let router = Router::new()
-    .route("/", get(root_get));
+    .route("/", get(root_get))
+    .with_state(AppState {
+        sys: Arc::new(Mutex::new(System::new())),
+    });
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:7032").await.unwrap();
-    axum::serve(listener, router).await.unwrap();
+    axum::serve(listener, router).await;
 
 }
 
-async fn root_get() -> &'static str{
-    "Hi from Axum"
+#[derive(Clone)]
+struct AppState {
+    sys: Arc<Mutex<System>>
 }
+
+async fn root_get(State(state): State<AppState>) -> String{
+    use std::fmt::Write;
+
+
+    let mut s = String::new();
+    let mut sys = state.sys.lock().unwrap();
+    sys.refresh_cpu();
+
+    for (i, cpu) in sys.cpus().iter().enumerate() {
+        let i = i+1;
+        let usage = cpu.cpu_usage();
+
+        writeln!(&mut s, "CPU {i} {usage}%");
+    }
+
+    s
+}
+
